fa6d0ab999b6f2265894ca55fccb92ef017cf89e
setalle <75754103+setalle@users.noreply.github.com>
2021-05-20 08:57:45 +0200

    -

--- other_spreadsheets/_makeindex.sh
+++ other_spreadsheets/_makeindex.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ls | egrep '(cup|cupx)' | perl -e 'print "<html><body><ul>"; while(<>) { chop $_; print "<li><a href=\"./$_\">$_</a></li>";} print "</ul></body></html>"' > index.html
+ls | egrep '(xl)' | perl -e 'print "<html><body><ul>"; while(<>) { chop $_; print "<li><a href=\"./$_\">$_</a></li>";} print "</ul></body></html>"' > index.html
